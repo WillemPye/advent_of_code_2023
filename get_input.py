20b42f83ff8e73e9622bdd780fe219f411c1670b
Willem Pye <willempye@gmail.com>
2023-12-03 12:23:16 +0000

    added cookies caching

--- get_input.py
+++ get_input.py
@@ -4,6 +4,46 @@ import argparse
 import json
 import csv
 import requests
+import hashlib
+import base64
+from cryptography.fernet import Fernet
+
+
+def make_cache(cookies, key):
+    # try:
+    if key is not None:
+        key = base64.b64encode(hashlib.sha256(key.encode()).hexdigest().encode())[-45:-1]
+        fernet = Fernet(key)
+        cookies = fernet.encrypt(cookies.encode()).decode()
+        print(cookies)
+
+    with open(".cookies_cache","w") as f:
+        f.write(cookies)
+    # except Exception as e:
+        # print(f"Caching cookies failed: {e}")
+
+def read_cache(key):
+    try:
+        with open(".cookies_cache","r") as f:
+            cookies = f.read()
+        
+        if key is not None:
+            key = base64.b64encode(hashlib.sha256(key.encode()).hexdigest().encode())[-45:-1]
+            fernet = Fernet(key)
+            cookies = fernet.decrypt(cookies).decode()
+        
+        return cookies
+    
+    except Exception as e:
+        print(f"Reading cookies failed: {e}")
+        cookies = input("Enter cookies manually\n")
+        if cookies == "":
+            sys.exit(1)
+        key = input("Enter password to encrypt cache with optional")
+        if key == "":
+            key = None
+        make_cache(cookies, key)
+        return cookies
 
 def create_directories(path):
     try:
@@ -85,24 +125,36 @@ def main():
     #set up arg parser
     parser = argparse.ArgumentParser(description="Get daily advent of code inputs")
     parser.add_argument("--day", required=True, help="Day of data you want")
-    parser.add_argument("--save_loc", required=True, help="Save name, without file extension.")
-    parser.add_argument("--cookies", required=True, help="Cookies argument for authentication, copy and paste from chrome dev tools")
+    parser.add_argument("--save_loc", required=False, help="Save name, without file extension.")
+    parser.add_argument("--cookies", required=False, help="Cookies argument for authentication, copy and paste from chrome dev tools")
     parser.add_argument("--format", required=False, help="Output format .txt, .json or .csv")
+    parser.add_argument("--key", required=False, help="passcode for reading cookies cache")
 
     # Parse the command line arguments
     args = parser.parse_args()
     day = args.day
     save_loc = args.save_loc
     cookies = args.cookies
+    key = args.key
     
     format = "txt" if args.format is None else args.format
     url = f"https://adventofcode.com/2023/day/{day}/input"
+
+    if save_loc is None:
+        save_loc = f"day{day}{os.path.sep}input"
     
     name_len = len(save_loc.split(os.path.sep)[-1])
     dir = save_loc[:-name_len]
     create_directories(dir)
 
     #Handle cookies
+    if cookies is None:
+        cookies = read_cache(key)
+    else:
+        _cache = input("Cache cookies? 'n' for No, anything else for Yes\n")
+        if _cache != "n":
+            make_cache(cookies, key)
+    
     cookies = cookies.replace(" ","")
     cookies = cookies.split(";")
     cookies = {k:v for cookie in cookies for k,v in [cookie.split("=",1)]}
